aef7ff5f386f439c91138017d610e93a0a4179f8
LinkKelly <54011287+LinkGoesBowling@users.noreply.github.com>
2026-06-11 11:56:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -189,6 +189,7 @@ function addStrike(){
 	if (shot === 17){
 		shot17Count = 10;
 	}
+	return;
 	}
 	else if (shot === 2 || shot === 4 || shot === 6 || shot === 8 || shot === 10 || shot === 12 || shot === 14 || shot === 16 || shot === 18){
 		console.log("It's the second shot! Click the spare button instead.");
