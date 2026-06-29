728d9b2c4b888b56ef42c2360c4e21da7f9e4ae9
LinkKelly <54011287+LinkGoesBowling@users.noreply.github.com>
2026-06-28 21:07:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -317,6 +317,12 @@ function addStrike(){
 function addSpare(){
 	spareButtonPressed = true;
 	spareButtonPressed = false;
+	if (shot === 2){
+		let changeShot2 = document.getElementById("shot2");
+		changeShot2.textContent = "/";
+		console.log("changeShot2 triggered"); //for debugging purposes
+	}
+		//continue here
 	if (shot === 22){
 		if (shot21Count === 10){
 			console.log("That's not a spare situation!");
@@ -347,12 +353,6 @@ function addSpare(){
 	else if (shot === 1 || shot === 3 || shot === 5 || shot === 7 || shot === 9 || shot === 11 || shot === 13 || shot === 15 || shot === 17 || shot === 20){
 		console.log("That's the first shot of the frame!");
 	}
-	if (shot === 2){
-		let changeShot2 = document.getElementById("shot2");
-		changeShot2.textContent = "/";
-		console.log("changeShot2 triggered"); //for debugging purposes
-	}
-		//continue here
 	if (shot === 21) {
 		if (previousShot === 10){
 			score += (10 - shot20Count) * 2;
